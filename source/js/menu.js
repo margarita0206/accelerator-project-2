741592d5e286c512567f03115df85a0f0734b89a
Маргарита Смирнова <158091165+margarita0206@users.noreply.github.com>
2025-01-09 20:36:57 +0300

    footer-social

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,18 +1,18 @@
-const navMain = document.querySelector('.header__container');
-const navToggle = document.querySelector('.header__button-burger');
+const navMain = document.querySelector('.header__wrapper');
+const navToggle = document.querySelector('.header__toggle');
 const body = document.querySelector('.page-body');
-const navLinks = document.querySelectorAll('.header-nav__link');
+const navLinks = document.querySelectorAll('.header__link');
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const menuToggle = () => {
   navToggle.onclick = function () {
-    navMain.classList.toggle('header__container--opened');
+    navMain.classList.toggle('header__wrapper--opened');
     body.classList.toggle('page-body--no-scroll');
   };
 
   document.addEventListener('keydown', (evt) => {
     if (isEscapeKey(evt)) {
-      navMain.classList.remove('header__container--opened');
+      navMain.classList.remove('header__wrapper--opened');
       body.classList.remove('page-body--no-scroll');
     }
   });
@@ -21,10 +21,10 @@ const menuToggle = () => {
 const navLinkToggle = () => {
   for (let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener('click', () => {
-      navMain.classList.remove('header__container--opened');
+      navMain.classList.remove('header__wrapper--opened');
       body.classList.remove('page-body--no-scroll');
     });
   }
 };
 
-export { menuToggle, navLinkToggle }
+export { menuToggle, navLinkToggle };
